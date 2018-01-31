d48d23dd9cfaa2601232da62da4512c17ab4d00a
Alban Dumas <alban.dumas@outlook.com>
2018-01-31 12:41:22 -0500

    Test commit.

--- test/JudicialFilePost.spec.js
+++ test/JudicialFilePost.spec.js
@@ -1,10 +1,10 @@
-var expect = require('chai').expect;
-import JudicialFilePost from '../src/api/judicialfile/JudicialFilePost';
-let judicialFilePost = new JudicialFilePost();
+// var expect = require('chai').expect;
+// import JudicialFilePost from '../src/api/judicialfile/JudicialFilePost';
+// let judicialFilePost = new JudicialFilePost();
 
-describe('JudicialFile POST API: GetFiles', function() {
-    it('should convert single digits', function() {
-        var result = judicialFilePost.GetFiles();
-        expect(result).to.equal('1');
-    });
-});
+// describe('JudicialFile POST API: GetFiles', function() {
+//     it('should convert single digits', function() {
+//         var result = judicialFilePost.GetFiles();
+//         expect(result).to.equal('1');
+//     });
+// });
