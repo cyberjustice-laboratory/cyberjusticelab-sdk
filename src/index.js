d5d485ba3e27fc7c19b7ef6d6cec8819850153fc
Alban Dumas <alban.dumas@outlook.com>
2018-04-25 17:28:25 -0400

    New apis exported.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 // Imports
 import config from "./config";
-import judicialFileApi from "./api/judicialFile";
+import judicialFile from "./api/judicialFile";
+import judicialEntity from "./api/judicialEntity";
+import document from "./api/document";
 
 // Get base configuration
 const initConfig = config;
 
 // Exports
 export default {
-  judicialFileApi: authState => judicialFileApi(initConfig, authState)
+  judicialFileApi: authState => judicialFile(initConfig, authState),
+  judicialEntityApi: authState => judicialEntity(initConfig, authState),
+  documentApi: authState => document(initConfig, authState)
 };
