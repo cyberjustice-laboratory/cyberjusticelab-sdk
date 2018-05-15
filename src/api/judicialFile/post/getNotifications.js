741ea15f3999924a27dd6ad2c9ca5032736101d7
olivierrivard <olivier@ordigraphe.com>
2018-05-15 13:33:33 -0400

    Add get notifications

---
+++ src/api/judicialFile/post/getNotifications.js
@@ -0,0 +1,17 @@
+/**
+ * Get a list of files.
+ * @prop {Object} config
+ * @prop {Object} requestHelper
+ * @param {Object} model
+ * @return {Object} List of files.
+ */
+export default async function(model) {
+  try {
+    return await this.requestHelper.post({
+      url: `${this.config.baseUrlApi}JudicialFile/POST/Notifications`,
+      data: Object.assign({}, this.config.entity, model)
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+}
--- src/api/judicialFile/post/index.js
+++ src/api/judicialFile/post/index.js
@@ -1,5 +1,6 @@
 // Imports
 import getFiles from "./getFiles";
+import getNotifications from "./getNotifications";
 import getFile from "./getFile";
 import createFile from "./createFile";
 import attachRepresentativesToParty from "./attachRepresentativesToParty";
@@ -8,5 +9,6 @@ export default api => ({
   getFiles: getFiles.bind(api),
   getFile: getFile.bind(api),
   createFile: createFile.bind(api),
-  attachRepresentativesToParty: attachRepresentativesToParty.bind(api)
+  attachRepresentativesToParty: attachRepresentativesToParty.bind(api),
+  getNotifications: getNotifications.bind(api)
 });
