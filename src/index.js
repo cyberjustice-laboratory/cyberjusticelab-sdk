05d0a3b5bd2094e0a3f6b35e776d5c680d280811
Alban Dumas <alban.dumas@outlook.com>
2018-05-29 14:03:19 -0400

    Refactor sdk config.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 // Imports
-import initConfig from "./config";
+import defaultConfig from "./config";
 import judicialFile from "./api/judicialFile";
 import judicialEntity from "./api/judicialEntity";
 import document from "./api/document";
 import calendar from "./api/calendar";
 
+const getConfig = config => Object.assign({}, defaultConfig, config);
+
 // Exports
 export default {
-  judicialFileApi: (config, authState) => judicialFile(Object.assign({}, initConfig, config), authState),
-  judicialEntityApi: (config, authState) => judicialEntity(Object.assign({}, initConfig, config), authState),
-  documentApi: (config, authState) => document(Object.assign({}, initConfig, config), authState),
-  calendarApi: (config, authState) => calendar(Object.assign({}, initConfig, config), authState)
+  judicialFileApi: (config, authState) => judicialFile(getConfig(config), authState),
+  judicialEntityApi: (config, authState) => judicialEntity(getConfig(config), authState),
+  documentApi: (config, authState) => document(getConfig(config), authState),
+  calendarApi: (config, authState) => calendar(getConfig(config), authState)
 };
