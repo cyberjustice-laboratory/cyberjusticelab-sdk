c10a7a658c617fabc56e94df188ae8ecc0013fe5
Mihai Ionescu <mihai.ionescu@cyberjustice.ca>
2018-05-30 10:06:32 -0400

    Rewrote the call function with sync await syntax

--- src/helpers/RequestHelper.js
+++ src/helpers/RequestHelper.js
@@ -25,22 +25,36 @@ export default class RequestHelper {
     }
   }
 
-  call(opts, methodType) {
-    return new Promise((resolve, reject) => {
-      axios({
+  // call(opts, methodType) {
+  //   return new Promise((resolve, reject) => {
+  //     axios({
+  //       method: methodType,
+  //       url: opts.url,
+  //       headers: this.defaultHeaders,
+  //       data: opts.data,
+  //       params: opts.params
+  //     })
+  //       .then(response => {
+  //         resolve(response);
+  //       })
+  //       .catch(error => {
+  //         reject(new Error(error));
+  //       });
+  //   });
+  // }
+
+  async call(opts, methodType) {
+    try {
+      return await axios({
         method: methodType,
         url: opts.url,
         headers: this.defaultHeaders,
         data: opts.data,
         params: opts.params
-      })
-        .then(response => {
-          resolve(response);
-        })
-        .catch(error => {
-          reject(new Error(error));
-        });
-    });
+      });
+    } catch (err) {
+      throw new Error(err);
+    }
   }
 
   /**
