6d406f6863cbf9c4ecc583b7de58c25ca7837383
Mihai Ionescu <mihai.ionescu@cyberjustice.ca>
2018-05-31 10:15:33 -0400

    Added higher order function to process each api methods

--- src/index.js
+++ src/index.js
@@ -5,10 +5,22 @@ import judicialEntity from "./api/judicialEntity";
 import document from "./api/document";
 import calendar from "./api/calendar";
 
-// Exports
-export default {
-  judicialFileApi: (appConfig, authState) => judicialFile(buildConnect(appConfig, authState)),
-  judicialEntityApi: (appConfig, authState) => judicialEntity(buildConnect(appConfig, authState)),
-  documentApi: (appConfig, authState) => document(buildConnect(appConfig, authState)),
-  calendarApi: (appConfig, authState) => calendar(buildConnect(appConfig, authState))
+const rawAPIs = {
+  judicialFile,
+  judicialEntity,
+  document,
+  calendar
 };
+
+// Each method has the right confiugations and helper functions.
+const sdkConnectedAPIs = (APIs => {
+  const connectedAPIs = {};
+  Object.keys(APIs).forEach(key => {
+    const api = APIs[key];
+    connectedAPIs[`${key}Api`] = (sdkConfig, authState) => api(buildConnect(sdkConfig, authState));
+  });
+  return connectedAPIs;
+})(rawAPIs);
+
+// Exports
+export default sdkConnectedAPIs;
